b1d49acfcf20a595402be5b527142b67b3196717
Hugo Osvaldo Barrera <hugo@barrera.io>
2016-03-03 22:10:04 -0300

    Add command to add new services

--- totp.rb
+++ totp.rb
@@ -38,6 +38,30 @@ class GenerateCmd < CmdParse::Command
   end
 end
 
+class AddServiceCmd < CmdParse::Command
+  def initialize
+    super 'add'
+    short_desc "Adds a new service (and its code) to pass's storage"
+    takes_commands false
+    argument_desc service: 'A name for the new service'
+    argument_desc code: 'The 2fa code provided by the service provider'
+  end
+
+  def execute service, code
+    command = "pass insert 2fa/#{service}/code"
+
+    Open3.popen3 command do | stdin, stdout, stderr, wait |
+      stdin.puts "#{code}\n#{code}"
+      stdin.close
+      err = stderr.read
+      unless err.chomp.empty?
+        warn err
+        exit 2
+      end
+    end
+  end
+end
+
 parser = CmdParse::CommandParser.new(handle_exceptions: :no_help)
 parser.add_command(CmdParse::HelpCommand.new)
 # parser.add_command(CmdParse::VersionCommand.new)
