31275c9018126f680e5d9eb6972d3ab3d21c8304
Hugo Osvaldo Barrera <hugo@barrera.io>
2016-03-03 22:09:17 -0300

    Use cmdparse for parsing the command line
    Let's stop reinventing the wheel. This makes adding more commands a lot
    simpler.

--- totp.rb
+++ totp.rb
@@ -4,29 +4,43 @@
 
 require 'rotp'
 require 'open3'
+require 'cmdparse'
 
-if ARGV.size != 1
-  warn "Usage: #{File.basename(__FILE__)} SERVICE"
-  exit 1
-end
+class GenerateCmd < CmdParse::Command
+  def initialize
+    super 'generate'
+    short_desc 'Generate a code for a given service'
+    takes_commands false
+    argument_desc service: "Service for which the code is required"
+  end
 
-cmd = "pass 2fa/#{ARGV[0]}/code"
-pass = nil
+  def execute service
+    command = "pass 2fa/#{service}/code"
+    pass = nil
 
-Open3.popen3(cmd) do | stdin, stdout, stderr, wait |
-  pass = stdout.read
-  err = stderr.read
-  unless err.chomp.empty?
-    puts err
-    exit 2
+    Open3.popen3 command do | stdin, stdout, stderr, wait |
+      pass = stdout.read
+      err = stderr.read
+      unless err.chomp.empty?
+        warn err
+        exit 2
+      end
+    end
+
+    totp = ROTP::TOTP.new pass
+    token = totp.now
+    puts token
+
+    Open3.popen3 "xclip" do | stdin, stdout, stderr, wait |
+      stdin.puts token.chomp
+      stdin.close
+    end
   end
 end
 
-totp = ROTP::TOTP.new(pass)
-token = totp.now
-puts token
-
-Open3.popen3("xclip") do | stdin, stdout, stderr, wait |
-  stdin.puts(token.chomp)
-  stdin.close
-end
+parser = CmdParse::CommandParser.new(handle_exceptions: :no_help)
+parser.add_command(CmdParse::HelpCommand.new)
+# parser.add_command(CmdParse::VersionCommand.new)
+parser.add_command(GenerateCmd.new, default: true)
+parser.add_command(AddServiceCmd.new, default: true)
+parser.parse
